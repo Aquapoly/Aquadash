e52e0dfe8ea3b6698ded0e4a5a0c7de967863694
Aezurly <66571923+Aezurly@users.noreply.github.com>
2025-02-05 16:57:11 -0500

    updated theme to match green aquapoly vibes

--- Aquadash-frontend/tailwind.config.js
+++ Aquadash-frontend/tailwind.config.js
@@ -6,6 +6,24 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dracula"],
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          primary: "#65c3c8",
+          "primary-content": "#223D30",
+          secondary: "#66cc8a",
+          "secondary-content": "#fff",
+        },
+      },
+      ,
+      {
+        dracula: {
+          ...require("daisyui/src/theming/themes")["dracula"],
+          primary: "#1DB88E",
+          secondary: "#3ac26c",
+        },
+      },
+    ],
   },
 };
