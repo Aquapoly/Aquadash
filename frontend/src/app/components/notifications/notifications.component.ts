37ee2ed96ba8dcda6231378360a0f9ec61933b0d
Aezurly <alice.vergeau@gmail.com>
2025-04-16 13:52:20 -0400

    notification ui + different notifications type

--- frontend/src/app/components/notifications/notifications.component.ts
+++ frontend/src/app/components/notifications/notifications.component.ts
@@ -1,6 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Notification, mockNotifications } from '@app/interfaces/notification';
+import {
+  Notification,
+  NotificationType,
+  mockNotifications,
+} from '@app/interfaces/notification';
 
 @Component({
   selector: 'app-notifications',
@@ -34,5 +38,41 @@ export class NotificationsComponent {
 
   get unreadNotifications(): Notification[] {
     return this.notifications.filter((n) => !n.read);
+  }
+
+  protected viewAllNotifications() {
+    console.log('TODO : rediriger vers la page de notifications');
+  }
+
+  getNotificationIcon(notification: Notification): {
+    icon: string;
+    classes: string;
+  } {
+    switch (notification.level) {
+      case NotificationType.Success:
+        return {
+          icon: 'M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z',
+          classes:
+            'text-green-500 bg-green-500 dark:bg-green-800 dark:text-green-200',
+        };
+      case NotificationType.Warning:
+        return {
+          icon: 'M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z',
+          classes:
+            'text-orange-500 bg-orange-500 dark:bg-orange-700 dark:text-orange-200',
+        };
+      case NotificationType.Error:
+        return {
+          icon: 'M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z',
+          classes: 'text-red-500 bg-red-500 dark:bg-red-800 dark:text-red-200',
+        };
+      case NotificationType.Info:
+      default:
+        return {
+          icon: 'M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z',
+          classes:
+            'text-blue-500 bg-blue-500 dark:bg-blue-800 dark:text-blue-200',
+        };
+    }
   }
 }
