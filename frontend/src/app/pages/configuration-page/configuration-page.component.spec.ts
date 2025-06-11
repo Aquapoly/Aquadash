37608e31c7a40036db255a9b66ab7ecd5ccbd401
niakouu <edelina.alieva@gmail.com>
2025-06-11 16:00:48 -0400

    Added karma configuration file and started fixing some tests except some related to backend's API.

--- frontend/src/app/pages/configuration-page/configuration-page.component.spec.ts
+++ frontend/src/app/pages/configuration-page/configuration-page.component.spec.ts
@@ -1,14 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ConfigurationPageComponent as ConfigurationPageComponent } from './configuration-page.component';
+import { HttpResponse, provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ApiService } from '@app/services/api.service';
+import { of } from 'rxjs/internal/observable/of';
 
 describe('ActuatorPageComponent', () => {
   let component: ConfigurationPageComponent;
   let fixture: ComponentFixture<ConfigurationPageComponent>;
+  let apiServiceSpy: jasmine.SpyObj<ApiService>;
 
   beforeEach(async () => {
+    // apiServiceSpy = jasmine.createSpyObj('ApiService', [
+    //   'getActuators', 'patchActuators'
+    // ]);
+
+    // apiServiceSpy.getActuators.and.returnValue(of(
+    //   {} as HttpResponse<Object>
+    // ));
+
+    // apiServiceSpy.patchActuators.and.returnValue(of(
+    //   {} as HttpResponse<Object>
+    // ));
+
     await TestBed.configureTestingModule({
       imports: [ConfigurationPageComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        // { provide: ApiService, useValue: apiServiceSpy },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ConfigurationPageComponent);
