94dc7dfc974180aeee0f3e29cc5f4839124759b2
AlbertVador <albert.vador@gmail.com>
2025-10-10 23:50:02 -0400

    add db savepoint

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,14 +2,16 @@ import pytest
 from sqlalchemy.orm import sessionmaker
 from fastapi.testclient import TestClient
 from app.main import app
-from app.database import Base, engine, get_db  # Import your existing setup
+from app.database import engine, get_db  # Import your existing setup
 
+# TODO Find best scope for fixtures
 @pytest.fixture
 def db_session():
     connection = engine.connect()
     transaction = connection.begin()
     Session = sessionmaker(bind=connection)
     session = Session()
+    session.begin_nested() # DB savepoint
     yield session
 
     session.close()
