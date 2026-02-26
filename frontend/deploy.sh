8a5e04796bbb9e194133113fb44d36d9bc9b3573
Aezurly <alice.vergeau@gmail.com>
2026-02-26 14:05:51 -0500

    old important changes in angular.json and package.json

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-npm run build && sudo rm -rf /var/www/aquadash && sudo cp -r dist/aquadash /var/www/aquadash
+npm run build && \
+sudo rm -rf /var/www/aquadash \
+&& sudo cp -r dist/aquadash /usr/share/nginx/html/
