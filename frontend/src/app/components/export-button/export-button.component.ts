751861396a7b3d2e7e554136cb2e19e920ac4338
André <wojcik.andre2.0@gmail.com>
2026-02-26 11:22:55 -0500

    feat: implemented exportMeasurements into export-button component

--- frontend/src/app/components/export-button/export-button.component.ts
+++ frontend/src/app/components/export-button/export-button.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Subscription } from 'rxjs';
 import { GlobalSettingsService } from '@app/services/global-settings.service/global-settings.service';
+import { ApiService } from '@app/services/api.service';
 import { LIGHT_THEME, THEME_COLOR } from '@app/constants/constants';
 
 @Component({
   selector: 'app-export-button',
   standalone: true,
-  imports: [FontAwesomeModule, CommonModule],
+  imports: [FontAwesomeModule, CommonModule, HttpClientModule],
   templateUrl: './export-button.component.html',
   styleUrl: './export-button.component.scss',
 })
@@ -18,7 +20,10 @@ export class ExportButtonComponent implements OnInit, OnDestroy {
   theme: string = LIGHT_THEME;
   private themeSubscription?: Subscription;
 
-  constructor(private readonly globalSettings: GlobalSettingsService) {}
+  constructor(
+    private readonly globalSettings: GlobalSettingsService,
+    private readonly apiService: ApiService
+  ) {}
 
   ngOnInit(): void {
     this.theme = this.globalSettings.getThemeName();
@@ -34,6 +39,19 @@ export class ExportButtonComponent implements OnInit, OnDestroy {
   }
 
   onClick() {
-    console.log("ca exporte");
+    this.apiService.exportMeasurements().subscribe({
+      next: (response) => {
+        const blob = response.body;
+        const url = window.URL.createObjectURL(blob!);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'all_sensors_measurements.csv';
+        link.click();
+        window.URL.revokeObjectURL(url);
+      },
+      error: (err) => {
+        console.error('Erreur lors de l\'export des données', err);
+      },
+    });
   }
 }
