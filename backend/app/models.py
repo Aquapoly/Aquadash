22f1635a6d3231894f344b8fd358c6b5946917d0
LucasVo-Dignard <vodignardlucas@gmail.com>
2026-03-14 11:48:08 -0400

    fix: oublie de faire le changement dans la contrainte

--- backend/app/models.py
+++ backend/app/models.py
@@ -48,12 +48,9 @@ class Sensor(Base):
     __table_args__ = (
         CheckConstraint("sensor_id>=0", name="check_id_sensor_positive"),
         CheckConstraint(
-            "(threshold_critically_low <= threshold_low AND "
+            "threshold_critically_low <= threshold_low AND "
             "threshold_low <= threshold_high AND "
-            "threshold_high <= threshold_critically_high) OR "
-            "(threshold_critically_low >= threshold_low AND "
-            "threshold_low >= threshold_high AND "
-            "threshold_high >= threshold_critically_high)",
+            "threshold_high <= threshold_critically_high",
             name="check_threshold_values"
         ),
     )
