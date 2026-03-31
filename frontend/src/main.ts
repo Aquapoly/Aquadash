adab77d84cf1d2ffc9fe414b252ed67231af77b5
Aezurly <alice.vergeau@gmail.com>
2026-03-31 19:28:57 -0400

    fix: hardcoded timezone

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from '@app/app.config';
 import { AppComponent } from '@app/pages/app/app.component';
+import * as Highcharts from 'highcharts';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+Highcharts.setOptions({
+  time: {
+    timezone: 'America/Toronto',
+  },
+});
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err),
+);
