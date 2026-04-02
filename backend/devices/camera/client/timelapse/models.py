b2c90bfdb0ae25a9edb4c1aa3d4dafa03d85f58b
Thierry-git <thierry.poulin000@gmail.com>
2026-04-02 19:31:57 -0400

    feat(timelapse models): added TimelapseMetadata model

--- backend/devices/camera/client/timelapse/models.py
+++ backend/devices/camera/client/timelapse/models.py
@@ -1,11 +1,17 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class TimelapseConfig(BaseModel):
     frequency: int  # seconds between captures
-    duration: int   # total duration in seconds
+    duration:  int  # total duration in seconds
     framerate: int  # frames per second for video assembly
 
 class TimelapseMetadata(BaseModel):
-    frames_taken: int
-    latest_frame_time: int
+    id: str
+    name: str
+    frames: int
+    framerate: float
+    start_date: datetime
+    last_frame_date: datetime
+    end_date: datetime
     ready: bool
